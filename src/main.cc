b2aa5be6c1d455b7c0701baefba10c417c57ca03
Nestal Wan <me@nestal.net>
2018-01-01 22:18:29 +0800

    Use boost program_options to parse command line

--- src/main.cc
+++ src/main.cc
@@ -5,25 +5,39 @@
 #include <boost/asio/strand.hpp>
 
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 #include <thread>
 #include <iostream>
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[])
 {
-    // Check command line arguments.
-    if (argc != 5)
-    {
-        std::cerr <<
-            "Usage: http-server-async <address> <port> <doc_root> <threads>\n" <<
-            "Example:\n" <<
-            "    http-server-async 0.0.0.0 8080 . 1\n";
-        return EXIT_FAILURE;
-    }
-    auto const address = boost::asio::ip::make_address(argv[1]);
-    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-    std::string const doc_root = argv[3];
-    auto const threads = std::max<int>(1, std::atoi(argv[4]));
+	// Declare the supported options.
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce help message")
+		("address,a", po::value<std::string>()->default_value("0.0.0.0"),    "IP address of the network interface to listener to")
+		("port,p",    po::value<unsigned short>()->default_value(80),        "Port number to listener to")
+		("root,r",    po::value<std::string>()->default_value("."),          "Album directory")
+		("threads",   po::value<int>()->default_value(1),                    "Number of threads")
+	;
+
+    po::variables_map config;
+	po::store(po::parse_command_line(argc, argv, desc), config);
+	po::notify(config);
+
+	if (config.count("help"))
+	{
+		std::cout << desc << "\n";
+		return 1;
+	}
+
+	auto const address = boost::asio::ip::make_address(config["address"].as<std::string>());
+    auto const port = config["port"].as<unsigned short>();
+    std::string const doc_root = config["root"].as<std::string>();
+    auto const threads = std::max<int>(1, config["threads"].as<int>());
 
     std::cout << address << " " << port << std::endl;
 
