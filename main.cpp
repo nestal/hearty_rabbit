06db344ae181fb55f46532a7611c382b91eeceec
Nestal Wan <me@nestal.net>
2018-01-01 18:02:08 +0800

    Hello world with beast

--- main.cpp
+++ main.cpp
@@ -1,11 +1,378 @@
-#include <iostream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/bind_executor.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/config.hpp>
+
 #include <boost/filesystem.hpp>
 
+#include <thread>
+#include <iostream>
 
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
 
-int main()
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template<
+    class Body, class Allocator,
+    class Send>
+void
+handle_request(
+    boost::beast::string_view doc_root,
+    http::request<Body, http::basic_fields<Allocator>>&& req,
+    Send&& send)
 {
-	boost::filesystem::path p;
-	std::cout << "Hello, World!" << p << std::endl;
-	return 0;
+    // Returns a bad request response
+    auto const bad_request =
+    [&req](boost::beast::string_view why)
+    {
+        http::response<http::string_body> res{http::status::bad_request, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = why.to_string();
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a not found response
+    auto const not_found =
+    [&req](boost::beast::string_view target)
+    {
+        http::response<http::string_body> res{http::status::not_found, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "The resource '" + target.to_string() + "' was not found.";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a server error response
+    auto const server_error =
+    [&req](boost::beast::string_view what)
+    {
+        http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "An error occurred: '" + what.to_string() + "'";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Make sure we can handle the method
+    if( req.method() != http::verb::get &&
+        req.method() != http::verb::head)
+        return send(bad_request("Unknown HTTP-method"));
+
+    // Request path must be absolute and not contain "..".
+    if( req.target().empty() ||
+        req.target()[0] != '/' ||
+        req.target().find("..") != boost::beast::string_view::npos)
+        return send(bad_request("Illegal request-target"));
+
+	std::string mime = "text/html";
+
+    boost::beast::error_code ec;
+    http::string_body::value_type body = "hello world!";
+//    body.open(path.c_str(), boost::beast::file_mode::scan, ec);
+
+    // Handle the case where the file doesn't exist
+    if(ec == boost::system::errc::no_such_file_or_directory)
+        return send(not_found(req.target()));
+
+    // Handle an unknown error
+    if(ec)
+        return send(server_error(ec.message()));
+
+    // Respond to HEAD request
+    if(req.method() == http::verb::head)
+    {
+        http::response<http::empty_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, mime);
+        res.content_length(body.size());
+        res.keep_alive(req.keep_alive());
+        return send(std::move(res));
+    }
+
+    auto body_size = body.size();
+	std::cout << "body size = " << body.size() << std::endl;
+
+    // Respond to GET request
+    http::response<http::string_body> res{
+        std::piecewise_construct,
+        std::make_tuple(std::move(body)),
+        std::make_tuple(http::status::ok, req.version())};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, mime);
+    res.content_length(body_size);
+    res.keep_alive(req.keep_alive());
+    return send(std::move(res));
+}
+
+// Report a failure
+void fail(boost::system::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// Handles an HTTP server connection
+class Session : public std::enable_shared_from_this<Session>
+{
+    // This is the C++11 equivalent of a generic lambda.
+    // The function object is used to send an HTTP message.
+    struct send_lambda
+    {
+        Session& self_;
+
+        explicit send_lambda(Session& self)
+            : self_(self)
+        {
+        }
+
+        template<bool isRequest, class Body, class Fields>
+        void
+        operator()(http::message<isRequest, Body, Fields>&& msg) const
+        {
+            // The lifetime of the message has to extend
+            // for the duration of the async operation so
+            // we use a shared_ptr to manage it.
+            auto sp = std::make_shared<
+                http::message<isRequest, Body, Fields>>(std::move(msg));
+
+            // Store a type-erased version of the shared
+            // pointer in the class to keep it alive.
+            self_.res_ = sp;
+
+            // Write the response
+            http::async_write(
+                self_.socket_,
+                *sp,
+                boost::asio::bind_executor(
+                    self_.strand_,
+                    std::bind(
+                        &Session::on_write,
+                        self_.shared_from_this(),
+                        std::placeholders::_1,
+                        std::placeholders::_2,
+                        sp->need_eof())));
+        }
+    };
+
+    tcp::socket socket_;
+    boost::asio::strand<
+        boost::asio::io_context::executor_type> strand_;
+    boost::beast::flat_buffer buffer_;
+    std::string doc_root_;
+    http::request<http::string_body> req_;
+    std::shared_ptr<void> res_;
+    send_lambda lambda_;
+
+public:
+    // Take ownership of the socket
+    explicit
+    Session(
+        tcp::socket socket,
+        std::string const& doc_root)
+        : socket_(std::move(socket))
+        , strand_(socket_.get_executor())
+        , doc_root_(doc_root)
+        , lambda_(*this)
+    {
+    }
+
+    // Start the asynchronous operation
+    void
+    run()
+    {
+        do_read();
+    }
+
+    void
+    do_read()
+    {
+        // Read a request
+        http::async_read(socket_, buffer_, req_,
+            boost::asio::bind_executor(
+                strand_,
+                std::bind(
+                    &Session::on_read,
+                    shared_from_this(),
+                    std::placeholders::_1,
+                    std::placeholders::_2)));
+    }
+
+    void
+    on_read(
+        boost::system::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        // This means they closed the connection
+        if(ec == http::error::end_of_stream)
+            return do_close();
+
+        if(ec)
+            return fail(ec, "read");
+
+        // Send the response
+        handle_request(doc_root_, std::move(req_), lambda_);
+    }
+
+    void
+    on_write(
+        boost::system::error_code ec,
+        std::size_t bytes_transferred,
+        bool close)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        if(ec)
+            return fail(ec, "write");
+
+        if(close)
+        {
+            // This means we should close the connection, usually because
+            // the response indicated the "Connection: close" semantic.
+            return do_close();
+        }
+
+        // We're done with the response so delete it
+        res_ = nullptr;
+
+        // Read another request
+        do_read();
+    }
+
+    void
+    do_close()
+    {
+        // Send a TCP shutdown
+        boost::system::error_code ec;
+        socket_.shutdown(tcp::socket::shutdown_send, ec);
+
+        // At this point the connection is closed gracefully
+    }
+};
+
+// Accepts incoming connections and launches the sessions
+class Listener : public std::enable_shared_from_this<Listener>
+{
+    tcp::acceptor m_acceptor;
+    tcp::socket m_socket;
+    std::string m_doc_root;
+
+public:
+    Listener(
+        boost::asio::io_context& ioc,
+        tcp::endpoint endpoint,
+        const std::string& doc_root)
+        : m_acceptor(ioc)
+        , m_socket(ioc)
+        , m_doc_root(doc_root)
+    {
+        boost::system::error_code ec;
+
+        // Open the acceptor
+        m_acceptor.open(endpoint.protocol(), ec);
+        if(ec)
+        {
+            fail(ec, "open");
+            return;
+        }
+
+        // Bind to the server address
+        m_acceptor.bind(endpoint, ec);
+        if(ec)
+        {
+            fail(ec, "bind");
+            return;
+        }
+
+        // Start listening for connections
+        m_acceptor.listen(
+            boost::asio::socket_base::max_listen_connections, ec);
+        if(ec)
+        {
+            fail(ec, "listen");
+            return;
+        }
+    }
+
+    // Start accepting incoming connections
+    void run()
+    {
+        if (m_acceptor.is_open())
+	        do_accept();
+    }
+
+    void do_accept()
+    {
+        m_acceptor.async_accept(
+            m_socket,
+            [self=shared_from_this()](boost::system::error_code ec){self->on_accept(ec);}
+		);
+    }
+
+    void on_accept(boost::system::error_code ec)
+    {
+        if(ec)
+        {
+            fail(ec, "accept");
+	        throw -1;
+        }
+        else
+        {
+            // Create the session and run it
+            std::make_shared<Session>(std::move(m_socket),m_doc_root)->run();
+        }
+
+        // Accept another connection
+        do_accept();
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    // Check command line arguments.
+    if (argc != 5)
+    {
+        std::cerr <<
+            "Usage: http-server-async <address> <port> <doc_root> <threads>\n" <<
+            "Example:\n" <<
+            "    http-server-async 0.0.0.0 8080 . 1\n";
+        return EXIT_FAILURE;
+    }
+    auto const address = boost::asio::ip::make_address(argv[1]);
+    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
+    std::string const doc_root = argv[3];
+    auto const threads = std::max<int>(1, std::atoi(argv[4]));
+
+    std::cout << address << " " << port << std::endl;
+
+    // The io_context is required for all I/O
+    boost::asio::io_context ioc{threads};
+
+    // Create and launch a listening port
+    std::make_shared<Listener>(
+	    ioc,
+        tcp::endpoint{address, port},
+        doc_root)->run();
+
+    // Run the I/O service on the requested number of threads
+    std::vector<std::thread> v;
+    v.reserve(static_cast<std::size_t>(threads - 1));
+    for(auto i = threads - 1; i > 0; --i)
+        v.emplace_back([&ioc]{ ioc.run(); });
+    ioc.run();
+
+    return EXIT_SUCCESS;
 }
