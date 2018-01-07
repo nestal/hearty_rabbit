67c94722fc96ef24d0df3dd84b193dc62b37b0cd
Nestal Wan <me@nestal.net>
2018-01-08 01:40:44 +0800

    Trying debug mode

--- automation/build.sh
+++ automation/build.sh
@@ -4,5 +4,5 @@ echo Building hearty_rabbit $BUILD_NUMBER
 
 mkdir build
 cd build
-cmake -DBOOST_ROOT=/build/boost_1_66_0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/hearty_rabbit ../hearty_rabbit
+cmake -DBOOST_ROOT=/build/boost_1_66_0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/opt/hearty_rabbit ../hearty_rabbit
 make -j4 package
