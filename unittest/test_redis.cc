ac30b7199d9ee4217518c70d75d99db8255813fc
Nestal Wan <me@nestal.net>
2018-01-10 23:21:13 +0800

    Async test seems working

--- unittest/test_redis.cc
+++ unittest/test_redis.cc
@@ -15,6 +15,7 @@
 #include <hiredis/hiredis.h>
 #include <hiredis/async.h>
 #include <iostream>
+#include <cassert>
 
 class RedisDriver
 {
@@ -24,6 +25,8 @@ public:
 		m_read{m_bic},
 		m_write{m_bic}
 	{
+		assert(m_ctx);
+
 		m_ctx->ev.data = this;
 		m_ctx->ev.addRead = RedisDriver::OnAddRead;
 		m_ctx->ev.delRead = RedisDriver::OnDelRead;
@@ -31,85 +34,120 @@ public:
 		m_ctx->ev.delWrite = RedisDriver::OnDelWrite;
 		m_ctx->ev.cleanup = RedisDriver::OnCleanUp;
 	}
+	~RedisDriver()
+	{
+		redisAsyncFree(m_ctx);
+	}
 
 private:
 	static void OnAddRead(void *pvthis)
 	{
 		auto pthis = reinterpret_cast<RedisDriver*>(pvthis);
-		pthis->m_read.assign(boost::asio::ip::tcp::v4(), pthis->m_ctx->c.fd);
-		pthis->DoRead();
+		std::cout << "OnAddRead()" << pthis->m_read.native_handle() << " " << pthis->m_ctx->c.fd << std::endl;
+
+		if (!pthis->m_read.is_open())
+		{
+			pthis->m_read.assign(boost::asio::ip::tcp::v4(), pthis->m_ctx->c.fd);
+			pthis->DoRead();
+		}
 	}
 	void DoRead()
 	{
-		m_read.async_wait(boost::asio::socket_base::wait_read, [this](const boost::system::error_code& ec2)
+		m_read.async_wait(boost::asio::socket_base::wait_read, [this](auto&& ec)
 		{
-			OnRead(ec2);
+			OnRead(ec);
 		});
 	}
 	void OnRead(const boost::system::error_code& ec)
 	{
+		m_read.release();
 		redisAsyncHandleRead(m_ctx);
 
-		if (!ec)
-			DoRead();
+//		if (!ec)
+//			DoRead();
 	}
 	static void OnDelRead(void *pvthis)
 	{
+		std::cout << "OnDelRead()" << std::endl;
+
 		auto pthis = reinterpret_cast<RedisDriver*>(pvthis);
 
-		boost::system::error_code ec;
-		pthis->m_read.cancel(ec);
+		if (pthis->m_read.is_open())
+		{
+			boost::system::error_code ec;
+			pthis->m_read.cancel(ec);
+			pthis->m_read.release();
+		}
 	}
 	static void OnAddWrite(void *pvthis)
 	{
 		auto pthis = reinterpret_cast<RedisDriver*>(pvthis);
 
+		std::cout << "OnAddWrite()" << pthis->m_write.native_handle() << " " << pthis->m_ctx->c.fd << std::endl;
+		if (!pthis->m_write.is_open())
+		{
+			pthis->m_write.assign(boost::asio::ip::tcp::v4(), pthis->m_ctx->c.fd);
+			pthis->DoWrite();
+		}
+	}
+	void DoWrite()
+	{
+		m_write.async_wait(boost::asio::socket_base::wait_write, [this](auto&& ec)
+		{
+			OnWrite(ec);
+		});
+	}
+	void OnWrite(const boost::system::error_code& ec)
+	{
+		m_write.release();
+		redisAsyncHandleWrite(m_ctx);
+
+//		if (!ec)
+//			DoWrite();
 	}
 	static void OnDelWrite(void *pvthis)
 	{
 		auto pthis = reinterpret_cast<RedisDriver*>(pvthis);
+		std::cout << "OnDelWrite()" << pthis->m_write.native_handle() << std::endl;
 
+		if (pthis->m_write.is_open())
+		{
+			boost::system::error_code ec;
+			pthis->m_write.cancel(ec);
+			pthis->m_write.release();
+		}
 	}
 	static void OnCleanUp(void *pvthis)
 	{
-		auto pthis = reinterpret_cast<RedisDriver*>(pvthis);
-
+		OnDelRead(pvthis);
+		OnDelWrite(pvthis);
 	}
 
 private:
 	boost::asio::io_context& m_bic;
 	boost::asio::ip::tcp::socket m_read, m_write;
+
+public:
 	redisAsyncContext       *m_ctx{redisAsyncConnect("localhost", 6379)};
 };
 
+void get_callback(redisAsyncContext *, void * r, void *)
+{
+    redisReply * reply = static_cast<redisReply *>(r);
+    if (reply)
+	    std::cout << "key: " << std::string_view{reply->str, (unsigned)reply->len} << std::endl;
+}
+
 int main()
 {
-	auto c = redisConnect("127.0.0.1", 6379);
-	if (!c || c->err)
-	{
-	    if (c)
-	    {
-	        printf("Error: %s\n", c->errstr);
-	        // handle error
-	    }
-	    else
-	    {
-	        printf("Can't allocate redis context\n");
-	    }
-	}
+	boost::asio::io_context ic;
 
-	auto reply = static_cast<redisReply*>(redisCommand(c, "set mycounter 1"));
-	switch (reply->type)
-	{
-		case REDIS_REPLY_STATUS:
-			std::cout << "Return status: " << std::string_view(reply->str, reply->len) << std::endl;
-			break;
-		case REDIS_REPLY_STRING:
-			std::cout << "Return string: " << std::string_view(reply->str, reply->len) << std::endl;
-			break;
-	}
+	RedisDriver redis{ic};
 
-	redisFree(c);
+	redisAsyncCommand(redis.m_ctx, nullptr, nullptr, "SET key 100");
+	redisAsyncCommand(redis.m_ctx, get_callback, nullptr, "GET key");
+
+	ic.run();
 
 	return 0;
 }
