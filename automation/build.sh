b41eadc7d7369d12952c5e972d152e819d88440a
Nestal Wan <me@nestal.net>
2018-01-10 22:33:55 +0800

    Added hiredis test program

--- automation/build.sh
+++ automation/build.sh
@@ -5,9 +5,6 @@ echo Building hearty_rabbit $BUILD_NUMBER
 # Try enabling devtoolset-7
 source scl_source enable devtoolset-7
 
-pwd
-ls
-
 cd docker-build
 cmake -DBOOST_ROOT=/build/boost_1_66_0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/opt/hearty_rabbit ../hearty_rabbit
 make -j8 package
