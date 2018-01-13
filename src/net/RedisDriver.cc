ae45e2cecf521f3ef27da13849d764d6f70dffc1
Nestal Wan <me@nestal.net>
2018-01-13 13:37:12 +0800

    Added libunwind

--- src/net/RedisDriver.cc
+++ src/net/RedisDriver.cc
@@ -16,7 +16,46 @@
 #include <cassert>
 #include <iostream>
 
+#include <boost/core/demangle.hpp>
+
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
+
 namespace hrb {
+
+// Call this function to get a backtrace.
+void backtrace()
+{
+	unw_cursor_t cursor;
+	unw_context_t context;
+
+	// Initialize cursor to current frame for local unwinding.
+	unw_getcontext(&context);
+	unw_init_local(&cursor, &context);
+
+	// Unwind frames one by one, going up the frame stack.
+	while (unw_step(&cursor) > 0)
+	{
+		unw_word_t offset, pc;
+		unw_get_reg(&cursor, UNW_REG_IP, &pc);
+		if (pc == 0)
+		{
+			break;
+		}
+		printf("0x%lx:", pc);
+
+		char sym[1024];
+		if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0)
+		{
+			auto demangled_sym = boost::core::demangle(sym);
+			printf(" (%s+0x%lx)\n", demangled_sym.c_str(), offset);
+		}
+		else
+		{
+			printf(" -- error: unable to obtain symbol name for this frame\n");
+		}
+	}
+}
 
 RedisDriver::RedisDriver(boost::asio::io_context& bic, const std::string& host, unsigned short port) :
 	m_bic{bic},
@@ -70,6 +109,11 @@ RedisDriver::RedisDriver(boost::asio::io_context& bic, const std::string& host, 
 		(pthis->m_ctx->ev.delRead)(pvthis);
 		(pthis->m_ctx->ev.delWrite)(pvthis);
 	};
+	::redisAsyncSetConnectCallback(m_ctx, [](const redisAsyncContext *ctx, int status)
+	{
+		hrb::backtrace();
+		abort();
+	});
 	::redisAsyncSetDisconnectCallback(m_ctx, [](const redisAsyncContext *ctx, int status)
 	{
 		std::cout << "error???" << ctx->errstr << std::endl;
