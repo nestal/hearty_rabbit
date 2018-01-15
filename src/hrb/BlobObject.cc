a557460715908169608531ea50e088998b0b93e8
Nestal Wan <me@nestal.net>
2018-01-15 23:42:14 +0800

    Designing the BlobObject class as a basic unit in hearty rabbit

---
+++ src/hrb/BlobObject.cc
@@ -0,0 +1,22 @@
+/*
+	Copyright © 2018 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the hearty_rabbit
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 1/15/18.
+//
+
+#include "BlobObject.hh"
+
+namespace hrb {
+
+BlobObject::BlobObject(const boost::filesystem::path &path)
+{
+	// read the file and calculate the sha and mime_type
+}
+
+} // end of namespace
