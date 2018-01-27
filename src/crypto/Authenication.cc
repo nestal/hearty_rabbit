43c7599adb1a9c2aabcc465140591a802e2d8b79
Nestal Wan <me@nestal.net>
2018-01-27 18:06:30 +0800

    Use mt19937 to generate salt

--- src/crypto/Authenication.cc
+++ src/crypto/Authenication.cc
@@ -14,14 +14,23 @@
 
 #include "net/Redis.hh"
 #include "crypto/Random.hh"
+
 #include "Password.hh"
 #include <openssl/evp.h>
 #include <openssl/sha.h>
+#include <random>
 
 namespace {
-using Salt = std::array<char, 32>;
+
+union Salt
+{
+	std::array<std::uint64_t, 4> lls;
+	std::array<char, 32>     ucs;
+};
 const int min_iteration = 5000;
-}
+
+} // end of anonymous namespace
+
 
 namespace hrb {
 
@@ -37,8 +46,14 @@ void add_user(
 	std::function<void(std::string_view cookie, std::error_code)> completion
 )
 {
-	auto salt = secure_random<Salt>();
-	auto key = password.derive_key({salt.data(), salt.size()}, min_iteration);
+	// There is no need to use cryptographically secure random number to generate the
+	// salt because it is not a secret to the attacker.
+	thread_local std::mt19937_64 salt_generator{secure_random<std::uint64_t>()};
+
+	Salt salt;
+	std::generate(salt.lls.begin(), salt.lls.end(), std::ref(salt_generator));
+
+	auto key = password.derive_key({salt.ucs.data(), salt.ucs.size()}, min_iteration);
 
 	db.command(
 		[key, salt](auto reply, auto&& ec)
@@ -47,7 +62,7 @@ void add_user(
 		},
 		"HSETNX user:%b salt %b key %b",
 		username.data(), username.size(),
-		salt.data(), salt.size(),
+		salt.ucs.data(), salt.ucs.size(),
 		key.data(), key.size()
 	);
 }
