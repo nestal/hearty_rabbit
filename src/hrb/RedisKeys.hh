7a00cdfb3fd7d6e9d6b45bc7c66239c864104aec
Matchman <match41@users.sourceforge.net>
2018-02-01 15:00:18 +0800

    Fix Reply iterator and added test

--- src/hrb/RedisKeys.hh
+++ src/hrb/RedisKeys.hh
@@ -14,7 +14,7 @@
 namespace hrb {
 
 static const std::size_t object_id_size  = 64;
-static std::array<const char, 4> object_redis_key_prefix = {'b','l','o','b'};
+static std::array<unsigned char, 5> object_redis_key_prefix = {'b','l','o','b', ':'};
 using ObjectRedisKey = std::array<unsigned char, object_id_size + object_redis_key_prefix.size()>;
 
 } // end of namespace
