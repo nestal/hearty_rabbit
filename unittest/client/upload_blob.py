4e9e9fe2c9d76e3ad7e15687af95bf13f2bac865
Nestal Wan <me@nestal.net>
2018-03-10 22:51:47 +0800

    Added python login test

--- unittest/client/upload_blob.py
+++ unittest/client/upload_blob.py
@@ -1,7 +1,7 @@
 import http.client
+import http.cookies
 import ssl
 import unittest
-
 
 class NormalTestCase(unittest.TestCase):
 	def test_fetch_home_page(self):
@@ -12,16 +12,22 @@ class NormalTestCase(unittest.TestCase):
 
 		# check response content
 		data = r1.read()
-		print(data)
+		self.assertGreater(len(data), 0)
 
 		conn.close()
 
 	def test_login(self):
+		headers = {"Content-type": "application/x-www-form-urlencoded"}
+
 		conn = http.client.HTTPSConnection("localhost:4433", context=ssl._create_unverified_context())
-		conn.request("POST", "/login", "username=nestal&password=hello")
+		conn.request("POST", "/login", "username=nestal&password=hello", headers)
 		r1 = conn.getresponse()
 		self.assertEqual(r1.status, 303)
 		self.assertEqual(r1.getheader("Location"), "/view/nestal/")
+
+		cookie1 = cookies.SimpleCookie()
+		cookie1.load(r1.getheader("Set-Cookie"))
+
 		r1.read()
 		conn.close()
 
