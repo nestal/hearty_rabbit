368bcc22caffb0bd3e03eb32be8fdd69677a3aed
Nestal Wan <me@nestal.net>
2018-03-15 23:13:37 +0800

    Move BlobRequest functions to separate .cc file

---
+++ src/hrb/BlobRequest.cc
@@ -0,0 +1,52 @@
+/*
+	Copyright © 2018 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the hearty_rabbit
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 3/15/18.
+//
+
+#include "BlobRequest.hh"
+
+namespace hrb {
+
+BlobRequest::BlobRequest(BlobRequest&& other) :
+	m_requester{std::move(other.m_requester)}, m_target{std::move(other.m_target)}, m_url{m_target},
+	m_version{other.m_version}, m_etag{std::move(other.m_etag)}, m_body{std::move(other.m_body)}
+{
+}
+
+BlobRequest::BlobRequest(const BlobRequest& other) :
+	m_requester{other.m_requester}, m_target{other.m_target}, m_url{m_target},
+	m_version{other.m_version}, m_etag{other.m_etag}, m_body{other.m_body}
+{
+}
+
+BlobRequest& BlobRequest::operator=(BlobRequest&& other)
+{
+	BlobRequest tmp{std::move(other)};
+	swap(tmp);
+	return *this;
+}
+BlobRequest& BlobRequest::operator=(const BlobRequest& other)
+{
+	BlobRequest tmp{other};
+	swap(tmp);
+	return *this;
+}
+
+void BlobRequest::swap(BlobRequest& tmp)
+{
+	m_requester.swap(tmp.m_requester);
+	m_target.swap(tmp.m_target);
+	m_url = URLIntent{m_target};
+	m_version = tmp.m_version;
+	m_etag.swap(tmp.m_etag);
+	m_body.swap(tmp.m_body);
+}
+
+} // end of namespace
--- src/hrb/BlobRequest.hh
+++ src/hrb/BlobRequest.hh
@@ -15,7 +15,6 @@
 #include "ObjectID.hh"
 #include "URLIntent.hh"
 #include "net/Request.hh"
-#include "util/Log.hh"
 
 #include <string_view>
 
@@ -29,47 +28,18 @@ public:
 		m_requester{requester}, m_target{req.target()}, m_url{m_target}, m_version{req.version()},
 		m_etag{req[http::field::if_none_match]}
 	{
-		Log(LOG_NOTICE, "%1% request type %2%", m_target, typeid(Request).name());
 		if constexpr (std::is_same<std::remove_reference_t<Request>, StringRequest>::value)
 		{
-			Log(LOG_NOTICE, "post data in blob \'%1%\'", req.body());
 			if (req[http::field::content_type] == "application/x-www-form-urlencoded")
 				m_body = std::move(req.body());
 		}
 	}
-	BlobRequest(BlobRequest&& other) :
-		m_requester{std::move(other.m_requester)}, m_target{std::move(other.m_target)}, m_url{m_target},
-		m_version{other.m_version}, m_etag{std::move(other.m_etag)}, m_body{std::move(other.m_body)}
-	{
-	}
-	BlobRequest(const BlobRequest& other) :
-		m_requester{other.m_requester}, m_target{other.m_target}, m_url{m_target},
-		m_version{other.m_version}, m_etag{other.m_etag}, m_body{other.m_body}
-	{
-	}
+	BlobRequest(BlobRequest&& other);
+	BlobRequest(const BlobRequest& other);
+	BlobRequest& operator=(BlobRequest&& other);
+	BlobRequest& operator=(const BlobRequest& other);
 
-	BlobRequest& operator=(BlobRequest&& other)
-	{
-		BlobRequest tmp{std::move(other)};
-		swap(tmp);
-		return *this;
-	}
-	BlobRequest& operator=(const BlobRequest& other)
-	{
-		BlobRequest tmp{other};
-		swap(tmp);
-		return *this;
-	}
-
-	void swap(BlobRequest& tmp)
-	{
-		m_requester.swap(tmp.m_requester);
-		m_target.swap(tmp.m_target);
-		m_url = URLIntent{m_target};
-		m_version = tmp.m_version;
-		m_etag.swap(tmp.m_etag);
-		m_body.swap(tmp.m_body);
-	}
+	void swap(BlobRequest& tmp);
 
 	std::optional<ObjectID> blob() const {return hex_to_object_id(m_url.filename());}
 	std::string_view requester() const {return m_requester;}
