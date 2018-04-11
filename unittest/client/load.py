08c4f3629002f3be7d0e47d5d4cf1c502835b9bb
Nestal Wan <me@nestal.net>
2018-04-12 00:30:52 +0800

    Set permission after upload

--- unittest/client/load.py
+++ unittest/client/load.py
@@ -3,7 +3,7 @@ import os, sys
 import requests
 import json
 import urllib.parse
-from pathlib import Path
+from stat import *
 
 # TODO: read password from stdin to avoid being saved to command history
 site = "https://" + sys.argv[1]
@@ -27,7 +27,24 @@ if login_response.status_code != 200 or dest.cookies.get("id") == "":
 
 for root, dirs, files in os.walk(blob_dir):
 	for file in files:
-		upload_url = site + "/upload/" + user + os.path.join(root, file)[len(blob_dir):]
+		local_path = os.path.join(root, file)
+
+		upload_url = site + "/upload/" + user + local_path[len(blob_dir):]
 		print(upload_url)
-		with open(os.path.join(root, file), "rb") as file_src:
-			dest.put(upload_url, data=file_src)
+		with open(local_path, "rb") as file_src:
+			upload_response = dest.put(upload_url, data=file_src)
+
+			permission = "private"
+			stat_info = os.stat(local_path)
+			if S_IROTH & S_IMODE(stat_info.st_mode):
+				permission = "public"
+			elif S_IRGRP & S_IMODE(stat_info.st_mode):
+				permission = "shared"
+
+			print(file + " is " + permission + " " + oct(S_IMODE(stat_info.st_mode)) + " " + oct(S_IROTH))
+
+			dest.post(
+				site + upload_response.headers["Location"],
+				data="perm=" + permission,
+				headers={"Content-type": "application/x-www-form-urlencoded"}
+			)
