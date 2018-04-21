93e09a6dc78461839725031d4c8179589931675d
Nestal Wan <me@nestal.net>
2018-04-22 00:23:58 +0800

    Move code to SplitBuffers.cc

---
+++ src/net/SplitBuffers.cc
@@ -0,0 +1,50 @@
+/*
+	Copyright © 2018 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the hearty_rabbit
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 4/22/18.
+//
+
+#include "SplitBuffers.hh"
+
+namespace hrb {
+
+void SplitBuffers::value_type::extra(std::string_view needle, std::string&& extra)
+{
+	auto offset = needle.empty() ? m_top.npos : m_top.find(needle);
+
+	// if "needle" is not found, append "extra" at the end.
+	// i.e. m_top will remain unchanged, and "follow" in the new
+	// segment will be empty
+	m_segs.emplace_back(
+		std::move(extra),
+		offset != m_top.npos ?
+			m_top.substr(offset + needle.size()) :
+			std::string_view{}
+	);
+
+	// substr() works even when offset is npos
+	// if use remove_suffix(), need to check against npos ourselves
+	// less code -> fewer bugs
+	m_top = m_top.substr(0, offset);
+}
+
+SplitBuffers::const_buffers_type SplitBuffers::value_type::data() const
+{
+	const_buffers_type result{{m_top.data(), m_top.size()}};
+
+	for (auto&& seg : m_segs)
+	{
+		result.emplace_back(seg.extra.data(), seg.extra.size());
+		result.emplace_back(seg.follow.data(), seg.follow.size());
+	}
+
+	return result;
+}
+
+} // end of namespace
--- src/net/SplitBuffers.hh
+++ src/net/SplitBuffers.hh
@@ -30,8 +30,8 @@ namespace hrb {
 /// 2. The additional <script> tag.
 /// 3. The rest of the HTML file.
 ///
-/// Only the middle buffer can be assigned dynamically. The first and third buffers are specified by
-/// std::string_view
+/// Only the middle buffer can be assigned dynamically. The first and third buffer are specified by
+/// std::string_view.
 class SplitBuffers
 {
 public:
@@ -51,38 +51,8 @@ public:
 			extra(needle, std::string{xtra});
 		}
 
-		void extra(std::string_view needle, std::string&& extra)
-		{
-			auto offset = needle.empty() ? m_top.npos : m_top.find(needle);
-
-			// if "needle" is not found, append "extra" at the end.
-			// i.e. m_top will remain unchanged, and "follow" in the new
-			// segment will be empty
-			m_segs.emplace_back(
-				std::move(extra),
-				offset != m_top.npos ?
-					m_top.substr(offset + needle.size()) :
-					std::string_view{}
-			);
-
-			// substr() works even when offset is npos
-			// if use remove_suffix(), need to check against npos ourselves
-			// less code -> fewer bugs
-			m_top = m_top.substr(0, offset);
-		}
-
-		const_buffers_type data() const
-		{
-			const_buffers_type result{{m_top.data(), m_top.size()}};
-
-			for (auto&& seg : m_segs)
-			{
-				result.emplace_back(seg.extra.data(), seg.extra.size());
-				result.emplace_back(seg.follow.data(), seg.follow.size());
-			}
-
-			return result;
-		}
+		void extra(std::string_view needle, std::string&& extra);
+		const_buffers_type data() const;
 
 	private:
 		std::string_view    m_top;
