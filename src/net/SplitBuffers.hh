7192a39ca2345eed09145cebcda4917160b571d5
Nestal Wan <me@nestal.net>
2018-04-22 01:06:06 +0800

    Added inject_before/after/middle but not tested yet

--- src/net/SplitBuffers.hh
+++ src/net/SplitBuffers.hh
@@ -37,7 +37,7 @@ class SplitBuffers
 public:
 	using const_buffers_type = std::vector<boost::asio::const_buffer>;
 
-	enum class Option {inject_after, replace};
+	enum class Option {inject_before, inject_after, inject_middle, replace};
 
 	class value_type
 	{
