41c611cb46fa033dbccb4b7a5c3f0869a24280a3
Nestal Wan <me@nestal.net>
2018-05-13 00:53:42 +0800

    Added phash()

--- unittest/tests/image/OpenCV-UT.cc
+++ unittest/tests/image/OpenCV-UT.cc
@@ -16,8 +16,38 @@
 
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <bitset>
 
 using namespace hrb;
+
+std::uint64_t phash(cv::Mat image)
+{
+	// gray
+	if (image.channels() > 1)
+		cv::cvtColor(cv::Mat{image}, image, CV_BGR2GRAY);
+
+	std::bitset<64> hash;
+
+	cv::Mat img32;
+	resize(image, img32, {32, 32});
+	img32 = cv::Mat_<double>(img32);
+
+	cv::Mat dst;
+	dct(img32, dst);
+
+	dst = dst({1, 1, 8, 8});
+	auto mean_mat = mean(dst);
+
+	cv::Mat mask = (dst >= mean_mat[0]);
+	for (int i = 0; i<mask.rows; i++)
+		for (int j = 0; j<mask.cols; j++)
+			mask.at<uchar>(i, j) == 0 ?
+			    (hash[i*mask.cols + j] = '0') :
+		        (hash[i*mask.cols + j] = '1');
+
+	return hash.to_ullong();
+}
 
 TEST_CASE("open image with opencv", "[normal]")
 {
@@ -30,4 +60,15 @@ TEST_CASE("open image with opencv", "[normal]")
 	resize(mat, large, {}, 1.5, 1.5);
 	REQUIRE(large.rows == 192*1.5);
 	REQUIRE(large.cols == 160*1.5);
+
+	REQUIRE(phash(mat) == phash(large));
+}
+
+TEST_CASE("phash of resized lena should be the same as the original", "[normal]")
+{
+	auto lena = cv::imread((fs::path{__FILE__}.parent_path()/"lena.png").string(), cv::IMREAD_COLOR);
+	cv::Mat large;
+
+	resize(lena, large, {}, 2, 2);
+	REQUIRE(phash(lena) == phash(large));
 }
