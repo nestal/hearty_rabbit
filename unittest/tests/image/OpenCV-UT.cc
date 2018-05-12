477b496df8585267b8f46b149cd05b8224d0bdbd
Nestal Wan <me@nestal.net>
2018-05-13 00:41:45 +0800

    Added contrib module but not sure we should use them

--- unittest/tests/image/OpenCV-UT.cc
+++ unittest/tests/image/OpenCV-UT.cc
@@ -13,7 +13,9 @@
 #include <catch.hpp>
 
 #include "util/FS.hh"
+
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace hrb;
 
@@ -22,4 +24,10 @@ TEST_CASE("open image with opencv", "[normal]")
 	auto mat = cv::imread((fs::path{__FILE__}.parent_path()/"up_f_rot90.jpg").string(), cv::IMREAD_COLOR);
 	REQUIRE(mat.rows == 192);
 	REQUIRE(mat.cols == 160);
+
+	// enlarge image
+	cv::Mat large;
+	resize(mat, large, {}, 1.5, 1.5);
+	REQUIRE(large.rows == 192*1.5);
+	REQUIRE(large.cols == 160*1.5);
 }
