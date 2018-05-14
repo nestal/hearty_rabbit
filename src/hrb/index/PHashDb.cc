2838cc9e01eb13284efd1724c425c07dec4d9732
Nestal Wan <me@nestal.net>
2018-05-14 23:21:04 +0800

    Remove debug code in lua script

--- src/hrb/index/PHashDb.cc
+++ src/hrb/index/PHashDb.cc
@@ -24,22 +24,18 @@ void PHashDb::add(const ObjectID& blob, PHash phash)
 {
 	static const char lua[] = R"__(
 		local oids = redis.call('HGET', KEYS[1], ARGV[1])
-		local pos = 0
 		if not oids then oids = ARGV[2] else
-			pos = string.find(oids, ARGV[2], 1, true) -- use plain text search
+			local pos = string.find(oids, ARGV[2], 1, true) -- use plain text search
 			if not pos
 			then
 				oids = oids .. ARGV[2]
-				pos = -1
 			end
 		end
-		redis.call('HSET', KEYS[1], ARGV[1], oids)
-		return oids
+		return redis.call('HSET', KEYS[1], ARGV[1], oids)
 	)__";
 	m_db.command(
 		[](auto&& reply, auto err)
 		{
-			Log(LOG_DEBUG, "add() script reply integer: %1%", reply.as_int());
 			if (!reply)
 				Log(LOG_WARNING, "add() script reply: %1%", reply.as_error());
 		},
