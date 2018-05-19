e7ac90826670c6c5cf9a365f5240f69776d9e40d
Nestal Wan <me@nestal.net>
2018-05-19 22:18:57 +0800

    Remove commented code

--- src/image/JPEG.hh
+++ src/image/JPEG.hh
@@ -12,54 +12,11 @@
 
 #pragma once
 
-#include "util/Size2D.hh"
 #include "util/BufferView.hh"
-
 #include <opencv2/core.hpp>
-#include <exception>
-#include <string_view>
-#include <vector>
 
 namespace hrb {
 
 cv::Mat load_image(BufferView raw);
-/*
-class TurboBuffer;
 
-class JPEG
-{
-public:
-	class Exception : public std::exception
-	{
-	public:
-		explicit Exception(const char *msg) : m_msg{msg} {}
-		const char* what() const noexcept override {return m_msg;}
-
-	private:
-		const char *m_msg{};
-	};
-
-public:
-	JPEG(const void *jpeg_data, std::size_t jpeg_size, const Size2D& max_dim);
-	JPEG(JPEG&&) noexcept;
-	JPEG(const JPEG&) = default;
-	~JPEG() = default;
-
-	JPEG& operator=(JPEG&&) noexcept;
-	JPEG& operator=(const JPEG&) = default;
-
-	Size2D size() const {return m_size;}
-	TurboBuffer compress(int quality) const;
-
-private:
-	static Size2D select_scaling_factor(const Size2D& max, const Size2D& actual);
-
-private:
-	std::vector<unsigned char> m_yuv;
-
-	Size2D m_size;
-	int m_subsample{};
-	int m_colorspace{};
-};
-*/
 } // end of namespace hrb
