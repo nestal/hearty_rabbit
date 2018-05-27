fabd305bb388ef17c61b5bca2357a2976514111b
Nestal Wan <me@nestal.net>
2018-05-27 12:04:12 +0800

    Add Timestamp to CollEntry

--- src/hrb/CollEntry.cc
+++ src/hrb/CollEntry.cc
@@ -25,12 +25,12 @@ CollEntry::CollEntry(std::string_view redis_reply) : m_raw{redis_reply}
 
 std::string CollEntry::create(
 	Permission perm, std::string_view filename, std::string_view mime,
-	std::chrono::system_clock::time_point timestamp
+	Timestamp timestamp
 )
 {
 	auto json = nlohmann::json::object();
 	json.emplace("mime", std::string{mime});
-//	json.emplace("timestamp", timestamp);
+	json.emplace("timestamp", timestamp);
 
 	if (!filename.empty())
 		json.emplace("filename", std::string{filename});
@@ -56,6 +56,12 @@ std::string CollEntry::mime() const
 {
 	auto doc = nlohmann::json::parse(CollEntry::json(), nullptr, false);
 	return doc.is_discarded() ? "" : doc.value(nlohmann::json::json_pointer{"/mime"}, "");
+}
+
+Timestamp CollEntry::timestamp() const
+{
+	auto doc = nlohmann::json::parse(CollEntry::json(), nullptr, false);
+	return doc.is_discarded() ? Timestamp{} : doc.value(nlohmann::json::json_pointer{"/timestamp"}, Timestamp{});
 }
 
 Permission CollEntry::permission() const
