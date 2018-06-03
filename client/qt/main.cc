654706d33c542199d11484d722bf687986888c74
Nestal Wan <me@nestal.net>
2018-06-03 10:41:52 +0800

    Added CMakeLists and main.cc for Qt GUI client

---
+++ client/qt/main.cc
@@ -0,0 +1,18 @@
+/*
+	Copyright © 2018 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the hearty_rabbit
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 6/3/18.
+//
+
+#include <QMainWindow>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
