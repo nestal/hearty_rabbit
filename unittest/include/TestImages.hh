b9ca43aa05332b67c19001bb83a9411637097337
Nestal Wan <me@nestal.net>
2018-06-25 20:48:13 +0800

    Added TestImages.hh/cc

---
+++ unittest/include/TestImages.hh
@@ -0,0 +1,19 @@
+/*
+	Copyright © 2018 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the hearty_rabbit
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 6/25/18.
+//
+
+#pragma once
+
+#include "common/FS.hh"
+
+namespace hrb {
+	extern const fs::path test_images;
+}
---
+++ unittest/tests/image/TestImages.cc
@@ -0,0 +1,19 @@
+/*
+	Copyright © 2018 Wan Wai Ho <me@nestal.net>
+    
+    This file is subject to the terms and conditions of the GNU General Public
+    License.  See the file COPYING in the main directory of the hearty_rabbit
+    distribution for more details.
+*/
+
+//
+// Created by nestal on 6/25/18.
+//
+
+#include "TestImages.hh"
+
+namespace hrb {
+
+const fs::path test_images{fs::path{__FILE__}.parent_path()};
+
+}
