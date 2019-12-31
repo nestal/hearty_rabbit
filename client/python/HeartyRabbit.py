dd2956298a087d414f48003550b0979b32daa302
Nestal Wan <me@nestal.net>
2019-12-31 17:23:39 +0800

    Add python API to list collections and blobs

--- client/python/HeartyRabbit.py
+++ client/python/HeartyRabbit.py
@@ -1,4 +1,29 @@
 import requests
+import urllib.parse
+
+class Blob:
+	m_json = None
+	m_id = None
+
+	def __init__(self, id, json):
+		self.m_id = id
+		self.m_json = json
+
+	def filename(self):
+		return self.m_json["filename"]
+
+	def id(self):
+		return self.m_id
+
+class Collection:
+	m_json = None
+
+	def __init__(self, json):
+		self.m_json = json
+
+	def name(self):
+		return self.m_json["coll"]
+
 
 class Session:
 	m_site = "localhost:4433"
@@ -28,9 +53,28 @@ class Session:
 		if user is None:
 			user = self.m_user
 
-		print("user = {}".format(user))
 		response = self.m_session.get(self.m_site + "/query/collection?user=" + user + "&json")
 		if response.status_code != 200:
 			raise ValueError("cannot query album list: {0}".format(response.status_code))
 
-		return response.json()["colls"]
+		result = []
+		for coll in response.json()["colls"]:
+			result.append(Collection(coll))
+
+		return result
+
+	def list_blobs(self, collection, user = None):
+		if user is None:
+			user = self.m_user
+
+		response = self.m_session.get(self.m_site + "/api/" + user + "/" + urllib.parse.quote_plus(collection.name()))
+		if response.status_code != 200:
+			raise ValueError("cannot get blobs from collections \"{}\": {}".format(
+				collection.name(),
+				response.status_code)
+			)
+
+		blobs = []
+		for id, blob in response.json()["elements"].items():
+			blobs.append(Blob(id, blob))
+		return blobs;
--- unittest/python/test_client.py
+++ unittest/python/test_client.py
@@ -8,4 +8,9 @@ spec.loader.exec_module(hrb)
 subject=hrb.Session("localhost:4433")
 subject.login("sumsum", "bearbear")
 
-print(subject.list_collections())
+for coll in subject.list_collections():
+#	print("collection: {}".format(coll.m_json))
+
+	blobs = subject.list_blobs(coll)
+	for blob in blobs:
+		print("blob {}: {}".format(blob.id(), blob.filename()))
