63427112f9964c5bd1a56aab1b2b11122dc5b5d9
Nestal Wan <me@nestal.net>
2020-01-02 22:34:54 +0800

    Add hrbsync basic code

--- client/hrbsync/main.cc
+++ client/hrbsync/main.cc
@@ -11,3 +11,50 @@
 //
 
 #include "client/HRBClient.hh"
+#include "client/HRBClient.ipp"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ssl/context.hpp>
+
+#include <iostream>
+#include <fstream>
+
+using namespace hrb;
+using namespace std::chrono_literals;
+
+int main(int argc, char **argv)
+{
+	if (argc < 6)
+	{
+		std::cerr << "usage: " << argv[0] << " [server] [port] [collection] [username] [password]" << std::endl;
+		return -1;
+	}
+
+	boost::asio::io_context ioc;
+	ssl::context ctx{ssl::context::sslv23_client};
+
+	HRBClient client{ioc, ctx, argv[1], argv[2]};
+	client.login(argv[4], argv[5], [&client, coll=argv[3]](std::error_code ec)
+	{
+		if (!ec)
+			std::cout << "login success!" << std::endl;
+
+		client.list_collection(coll, [&client](Collection&& coll, std::error_code ec)
+		{
+			if (!ec)
+				for (auto&& [id, entry] : coll.blobs())
+				{
+					std::cout << "blob: " << to_hex(id) << " " << entry.filename << std::endl;
+
+					client.get_blob(coll.owner(), coll.name(), id, [fname=entry.filename](std::string&& body, std::error_code ec)
+					{
+						std::ofstream out{fname};
+						out.rdbuf()->sputn(body.data(), body.size());
+					});
+				}
+		});
+	});
+	ioc.run();
+
+	return 0;
+}
